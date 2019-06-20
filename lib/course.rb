3adb120fdd22da98638ab7054ab3feb69c391ec9
Santiago Sanchez <chagostwo@yahoo.com>
2019-06-20 02:25:39 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,27 @@
+class Course
+  attr_accessor :title, :schedule, :description
+  @@all =[]
 
+  def initialized
+    @@all << self
+  end
+  def title
 
+  end
+
+  def schedule
+
+  end
+
+  def description
+
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+end
