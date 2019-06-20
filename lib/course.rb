f50823f421ae4a3e6d5b8d823b13f99093f3371f
Santiago Sanchez <chagostwo@yahoo.com>
2019-06-20 03:22:42 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,29 @@
+class Course
+  attr_accessor :title, :schedule, :description
 
+  @@all =[]
 
+  def initialize
+    @@all << self
+  end
+
+  def title
+    @title = title
+  end
+
+  def schedule
+    @schedule = schedule
+  end
+
+  def description
+    @description = description
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+end
